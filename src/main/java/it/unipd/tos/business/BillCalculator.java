9d19655e92877f54c3b7932210797ca1d1fc60fa
nicolovergari <nicolo.vergari@studenti.unipd.it>
2018-12-13 12:28:59 +0100

    aggiunta la rimozione della pizza meno costosa e il relativo test di unità close #2

--- src/main/java/it/unipd/tos/business/BillCalculator.java
+++ src/main/java/it/unipd/tos/business/BillCalculator.java
@@ -15,8 +15,20 @@ public class BillCalculator implements RestaurantBill {
     public double getOrderPrice(List<MenuItem> itemsOrdered) throws RestaurantBillException {
         double billTot =0;
 
+        int count = 0;
+        double min = Integer.MAX_VALUE;
+
         for (MenuItem m : itemsOrdered) {
             billTot += m.getPrice();
+            if (m.getType() == MenuItem.itemType.PIZZE) {
+                count++;
+                if (m.getPrice() < min) {
+                    min = m.getPrice();
+                }
+            }
+        }
+        if (count > 10) {
+            billTot -= min;
         }
         return billTot;
     }
